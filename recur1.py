5f0507d309c59607cab4a1ce591bd0ea2eabfc81
2021115105 <126667294+2021115105@users.noreply.github.com>
2023-04-14 21:56:39 +0530

    Update recur1.py

--- recur1.py
+++ recur1.py
@@ -1,8 +1,9 @@
-def number(a,b):
-     if a==b:
-         print a
-         return
-     else:
-         print(a,end="")
-         return(number(a+2),b)
- number(0,10)
+def number(a, b):
+    if a == b:
+        print(a)
+        return
+    else:
+        print(a, end="")
+        return number(a+2, b)
+
+number(0, 10)
